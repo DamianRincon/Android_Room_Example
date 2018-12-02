165cde419c875d14caa82a2612c578a45b50090f
demian <damianrincondrc@gmail.com>
2018-12-02 03:30:29 -0600

    insert_commit

--- app/src/main/java/mx/rincon/damian/crudroom/MainActivity.java
+++ app/src/main/java/mx/rincon/damian/crudroom/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private ContactAdapter adapter;
     private ContactViewModel contactViewModel;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
